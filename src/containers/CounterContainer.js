db8be9f34e7fe8f49d4e9fe6bbc4870b9786dc1e
hsahn93 <devhyunsang@gmail.com>
2022-01-13 18:02:56 +0900

    redux-thunk 작성중 오류 발생...

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,27 +1,22 @@
-import React from 'react'
-import Counter from '../components/Counter'
-import { useSelector, useDispatch } from 'react-redux'
-import { increase, decrease } from '../modules/counter'
-
+import React from 'react';
+import Counter from '../components/Counter';
+import {useSelector, useDispatch} from 'react-redux';
+import {increaseAsync, decreaseAsync} from '../modules/counter';
 
 function CounterContainer() {
-  const number = useSelector(state => state.counter)
-  const dispatch = useDispatch()
+  const number = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   const onIncrease = () => {
-    dispatch(increase())
-  }
+    dispatch(increaseAsync());
+  };
   const onDecrease = () => {
-    dispatch(decrease())
-  }
-  return (
-    <Counter number={number} onDecrease={onDecrease} onIncrease={onIncrease} />
-  )
+    dispatch(decreaseAsync());
+  };
 
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
 }
 
-
-export default CounterContainer
-
-
-
+export default CounterContainer;
--- src/middleware/myLogger.js
+++ src/middleware/myLogger.js
@@ -1,14 +1,11 @@
-const myLogger = store => next => action => {
-  console.log(action)//액션출력
-  const result = next(action) // 다음 미들웨어 (또는 리듀서) 에게 액션을 전달함.
-  
-  //업데이트 이후의 상태를 조회함.
-  console.log('\t', store.getState())
-  
-  
-  return result //여기에서 반환하는 값은 dispatch(action) 의 결과
+const myLogger = (store) => (next) => (action) => {
+  console.log(action); // 먼저 액션을 출력합니다.
+  const result = next(action); // 다음 미들웨어 (또는 리듀서) 에게 액션을 전달합니다.
 
+  // 업데이트 이후의 상태를 조회합니다.
+  console.log('\t', store.getState()); // '\t' 는 탭 문자 입니다.
 
-}
+  return result; // 여기서 반환하는 값은 dispatch(action)의 결과물이 됩니다. 기본: undefined
+};
 
-export default myLogger
+export default myLogger;
